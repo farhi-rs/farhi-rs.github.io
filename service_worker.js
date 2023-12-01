6a9b692c79490e4a1554533e6a435e1cc160804b
Flame <flame.thebounegabsempire@gmail.com>
2023-12-01 15:20:41 +0100

    Add files via upload

--- service_worker.js
+++ service_worker.js
@@ -1,17 +1,20 @@
 // Cached core static resources 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(".").then(cache=>{
-      return cache.addAll(["./",'./anim/', './assets/imgs/', './fonts/', './fonts/symbols/', './lib/', './styling/']);
+    caches.open("farhi_rs").then(cache=>{
+      return cache.addAll(['anim/book.json', 'assets/imgs/site_icon.png', 'fonts/alexandria-latin.woff2', 'fonts/alexandria.woff2', 'fonts/poppins.woff2', 'fonts/symbols/material-symbols.woff2', 'lib/lottie-player.js', 'styling/material-symbols.css', 'styling/pagefont.css', 'favicon.ico', 'index.html', 'index.js', 'manifest.json', 'service_worker.js', 'style.css']);
     })
   );
 });
 
-// Fatch resources
+// Fetch resources
 self.addEventListener("fetch", e => {
+  console.log(e.request.url)
   e.respondWith(
-    caches.match(e.request).then(response=>{
-      return response || fetch(e.request);
+    caches.open("farhi_rs").then(cache => {
+      cache.match(e.request).then(response=>{
+        return response || fetch(e.request);
+      })
     })
   );
 });
