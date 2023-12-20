dc7a50b17b43ced276c80dec8a11f819b6b6fbf9
Farhi RS <flame.thebounegabsempire@gmail.com>
2023-12-20 04:16:32 +0100

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,7 @@
 // Cached core static resources 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.addAll(['anim/book.json', 'anim/loading.json', 'assets/imgs/site_icon.png', 'fonts/alexandria-latin.woff2', 'fonts/alexandria.woff2', 'fonts/poppins.woff2', 'fonts/symbols/material-symbols.woff2', 'lib/lottie-player.js', 'styling/material-symbols.css', 'styling/pagefont.css', 'favicon.ico', 'index.html', 'index.js', 'eruda.js', 'notification.js', 'style.css'])
+    caches.addAll(['anim/book.json', 'anim/loading.json', 'assets/imgs/site_icon.png', 'fonts/alexandria-latin.woff2', 'fonts/alexandria.woff2', 'fonts/poppins.woff2', 'fonts/symbols/material-symbols.woff2', 'lib/lottie-player.js', 'lib/xlsx.bundle.js', 'styling/material-symbols.css', 'styling/pagefont.css', 'favicon.ico', 'index.html', 'index.js', 'eruda.js', 'notification.js', 'style.css'])
   );
 });
 
